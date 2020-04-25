fa7fbcd9eb93073d0edcb5055b3ab44e3d1950c0
LiaoXiMing <1435646097@qq.com>
2020-04-25 22:14:03 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Home组件
+    <el-button type="info" @click="logout">退出登录</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+      this.$message.success('退出登录成功')
+    }
+  }
+}
+</script>
